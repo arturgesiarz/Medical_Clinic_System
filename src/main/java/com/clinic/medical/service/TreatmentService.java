0fd2b8693a5ff7bb6409b19e6a43f7df54d0cd01
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-03-27 13:43:59 +0100

    Added treatment works

--- src/main/java/com/clinic/medical/service/TreatmentService.java
+++ src/main/java/com/clinic/medical/service/TreatmentService.java
@@ -2,6 +2,7 @@ package com.clinic.medical.service;
 
 import com.clinic.medical.dto.TreatmentRequest;
 import com.clinic.medical.model.Treatment;
+import com.clinic.medical.repository.TreatmentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,14 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class TreatmentService {
+    private final TreatmentRepository treatmentRepository;
+
     public void add(TreatmentRequest treatmentRequest) {
+        Treatment treatment = Treatment.builder()
+                .treatmentName(treatmentRequest.getTreatmentName())
+                .treatmentPrice(treatmentRequest.getTreatmentPrice()).build();
+
+        treatmentRepository.save(treatment);
     }
 
     public void delete(Long patientID) {
